eaf1f223d15560fec47fd723cb3453d268494c4f
chaudhary-mukesh <153888197+chaudhary-mukesh@users.noreply.github.com>
2025-06-12 02:52:14 +0000

    datatypes and variables

---
+++ 01_basic/01_var_let.js
@@ -0,0 +1,14 @@
+const accountId = 421123;
+let accountEmail = "m1@gmail.com";
+var accountPassword = "123123";
+accountCity = "Mumbai";
+let accountDate;
+
+//accountId = 12;
+
+/*
+var are no longer in use so avoid it
+*/
+
+//console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountDate]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version
+console.log(3+3);
+
+let name ="Mukesh";
+let age = 23;
+let isLoggedIn = true;
+
+/*
+number
+bigint
+string
+boolean
+null
+undefined
+symbol
+object
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
